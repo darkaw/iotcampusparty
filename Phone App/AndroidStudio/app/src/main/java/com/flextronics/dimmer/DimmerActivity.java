308cb2e4585653fdcc4431a4f574c61f724bd9e5
Xavier Guzman <xaguzman@gmail.com>
2015-07-24 16:45:45 -0500

    agregado proyecto de android studio

---
+++ Phone App/AndroidStudio/app/src/main/java/com/flextronics/dimmer/DimmerActivity.java
@@ -0,0 +1,93 @@
+package com.flextronics.dimmer;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class DimmerActivity extends Activity {
+
+    TextView txtResponse;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        SeekBar bar = (SeekBar)findViewById(R.id.seekBar);
+        final TextView txt = (TextView)findViewById(R.id.txtValue);
+        txt.setText(String.valueOf(bar.getProgress()));
+        txtResponse = (TextView)findViewById(R.id.txtResponse);
+
+
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                txt.setText(String.valueOf(progress));
+//                txtResponse.setText("Please wait...");
+//                new SendRequestTask().execute(getString(R.string.host), String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                String progress = String.valueOf( seekBar.getProgress() );
+                txt.setText(progress);
+                txtResponse.setText("Please wait...");
+                new SendRequestTask().execute(getString(R.string.host), progress);
+            }
+        });
+
+    }
+
+    private class SendRequestTask extends AsyncTask<String, Void, String>{
+
+        private static final String token = "letmein";
+        private static final String target = "device1";
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String queryString = String.format("?intensity=%s&token=%s&device=%s", params[1], token, target);
+            String requestUrl = params[0] + queryString;
+
+            // params comes from the execute() call: params[0] is the url.
+            String res = "";
+            String responseCode ="" ;
+            try {
+                Log.d("Dimmer", "Sending request " + requestUrl);
+                HttpURLConnection urlConnection = (HttpURLConnection) new URL(requestUrl).openConnection();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ( (line = reader.readLine()) != null){
+                    builder.append(line);
+                }
+                res = builder.toString();
+                urlConnection.disconnect();
+                responseCode = urlConnection.getResponseMessage();
+            } catch (Exception e) {
+                responseCode = e.getMessage();
+            } finally{
+                return responseCode;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("Dimmer", "Response was " + result);
+            txtResponse.setText(result);
+        }
+    }
+}
